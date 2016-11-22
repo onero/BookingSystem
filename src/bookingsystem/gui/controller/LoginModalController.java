ed4f8ee1817514c84e0988f9bae62cd93f72fc9d
Adam Hansen <adamlars90@gmail.com>
2016-11-22 13:29:57 +0100

    Added login modal

---
+++ src/bookingsystem/gui/controller/LoginModalController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookingsystem.gui.controller;
+
+import bookingsystem.BookingSystem;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author gta1
+ */
+public class LoginModalController implements Initializable {
+
+    @FXML
+    private TextField userId;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button login;
+    @FXML
+    private Label errorMessage;
+
+    private BookingSystem application;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void processLogin(ActionEvent event) {
+        //Process login
+    }
+
+}
