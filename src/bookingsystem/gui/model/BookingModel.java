07e60362a5a13d2609daeb034f01371cad853b86
Adam Hansen <adamlars90@gmail.com>
2016-11-23 13:00:43 +0100

    Added entertainers

--- src/bookingsystem/gui/model/BookingModel.java
+++ src/bookingsystem/gui/model/BookingModel.java
@@ -41,6 +41,37 @@ public class BookingModel {
         mLanger.setDescription("Man he can sing!");
         mLanger.setIMAGE_PATH("/bookingsystem/assets/img/MadsLanger.jpg");
         entertainers.add(mLanger);
+
+        Entertainer aMatthesen = new Entertainer(
+                "Anders Matthesen", "aMatthesen@contact.dk", 12345678, 1500000, EntertainerType.STAND_UP);
+        aMatthesen.setDescription("The funniest man in the world");
+        aMatthesen.setIMAGE_PATH("/bookingsystem/assets/img/aMatthesen.jpg");
+        entertainers.add(aMatthesen);
+
+        Entertainer kikky = new Entertainer(
+                "Kikky", "kikky@contact.dk", 1234678, 30000, EntertainerType.EVENT_GIRL);
+        kikky.setDescription("Kikky the kinky girl...");
+        kikky.setIMAGE_PATH("/bookingsystem/assets/img/Kikky.jpg");
+        entertainers.add(kikky);
+
+        Entertainer scarlet = new Entertainer(
+                "Scarlet Johansson", "lækkertøs@hotmail.com", 2000000, 1500000, EntertainerType.EVENT_GIRL);
+        scarlet.setDescription("Verdens lækreste tøs med dejlige fødedygtige hofter.");
+        scarlet.setIMAGE_PATH("/bookingsystem/assets/img/sJohansson.jpg");
+        entertainers.add(scarlet);
+
+        Entertainer breum = new Entertainer(
+                "Morten Breum", "DjMaster@contact.dk", 50000, 35000, EntertainerType.DJ);
+        breum.setDescription("En af danmarks dygtigste Dj´s.");
+        breum.setIMAGE_PATH("/bookingsystem/assets/img/mBreum.jpg");
+        entertainers.add(breum);
+
+        Entertainer kato = new Entertainer(
+                "Kato", "Kato@contact.dk", 80000, 60000, EntertainerType.DJ);
+        kato.setDescription("Danmarks mest hotte dj!");
+        kato.setIMAGE_PATH("/bookingsystem/assets/img/Kato.jpg");
+        entertainers.add(kato);
+
     }
 
     public ObservableList<Entertainer> getEntertainers() {
